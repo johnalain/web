1b01ad8a8937660f8e59d5aaf74c34b1e7a85ddc
john alain <johnalain572@gmail.com>
2024-02-28 12:47:29 +0200

    tkinter windows

---
+++ createWindo.py
@@ -0,0 +1,16 @@
+#https://youtu.be/ARHo--pz0t4?list=PLSiLeKadTQ7nLJxpQo1-944miQKlheu-v
+from tkinter import *
+
+pro1=Tk()
+pro2=Tk()
+
+pro1.title('rita')
+pro2.title('josephine')
+
+pro1.geometry('300x600+10+10')
+pro2.geometry('800x300+400+10')
+
+pro1.config(background="green")
+pro2.config(background="red")
+
+pro1.mainloop()
---
+++ tkincrud.py
@@ -0,0 +1,13 @@
+from tkinter import *
+pro = Tk()
+pro.geometry('800x400+600+50')
+#pro.geometry('widthxheight+left+right')
+pro.resizable(True, True)
+pro.title('josephine rita')
+pro.config(background='red')
+pro.iconbitmap('//Users//michelkadi//Desktop//download (2).ico')
+pro.minsize(400,200)
+pro.maxsize(600,600)
+#https://youtu.be/5BCIc-sQla0?list=PLSiLeKadTQ7nLJxpQo1-944miQKlheu-v  video 7
+
+pro.mainloop()
